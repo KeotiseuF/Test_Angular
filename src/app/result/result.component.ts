445e0156c92e8c2517738639dd65bdb1b18046d7
KeotiseuF <curtis.fanchonna@gmail.com>
2022-12-31 15:27:43 +0100

    ajout des messages de fin

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModelizationQcm } from '../services/modelization-qcm.service';
 
 @Component({
@@ -6,8 +6,29 @@ import { ModelizationQcm } from '../services/modelization-qcm.service';
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.scss']
 })
-export class ResultComponent {
+export class ResultComponent implements OnInit {
+  wrongResponses = this.modelizationQcm.wrongResponses;
+  message!: string; 
+
   constructor(private modelizationQcm: ModelizationQcm){}
 
-  wrongResponses = this.modelizationQcm.wrongResponses;
+  ngOnInit(): void
+  {
+    if( this.wrongResponses >= 8 )
+    {
+      this.message = `Tu as été aussi perspicace qu'Eren lors de sa transformation en Titan dans ce QCM, 
+      et cela a payé ! Félicitations pour ton excellent score.`
+    }
+    else if( this.wrongResponses >= 5 )
+    {
+      this.message = `Tu as presque été aussi précis mais autant déterminé qu'un membre de l'escouade d'exploration dans ce QCM, 
+      continue comme cela! Félicitations pour ton score.`
+    }
+    else
+    {
+      this.message = `Ne sois pas découragé par ton score dans ce QCM, tu as encore de nombreuses opportunités pour t'améliorer et 
+      devenir un combattant redoutable comme Mikasa. 
+      Félicitations pour ta détermination et ta motivation à apprendre.`
+    }
+  }
 }
